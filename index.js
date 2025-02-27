ad5e8ed9fddc29d6e38bb818918f960cfc411f9d
Abhishek-B-R <byteblaster1468@gmail.com>
2025-02-27 08:59:40 +0530

    changed as per new update

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function delay(time) {
   await page.keyboard.press('Enter');
 
   await page.click('#yyyy'); 
-  const yearIndex = fileData.yyyy - 1973; 
+  const yearIndex = fileData.yyyy - 1964; 
   for (let i = 0; i < yearIndex; i++) {
     await page.keyboard.press('ArrowDown');
     if(i < 37){
@@ -65,53 +65,53 @@ function delay(time) {
 
   console.log('Filled form fields with credentials');
 
-  const imageSrc = await page.evaluate(() => {
-    const image = document.querySelector('.uk-margin img');
-    return image ? image.src : null;
+  // const imageSrc = await page.evaluate(() => {
+  //   const image = document.querySelector('.uk-margin img');
+  //   return image ? image.src : null;
+  // });
+
+  // if (imageSrc) {
+  //   console.log('Captcha image found:', imageSrc);
+
+  //   const captchaImagePath = path.join(__dirname, 'captcha.png');
+  //   const writer = fs.createWriteStream(captchaImagePath);
+  //   const response = await axios({
+  //     url: imageSrc,
+  //     method: 'GET',
+  //     responseType: 'stream'
+  //   });
+
+  //   response.data.pipe(writer);
+
+  //   await new Promise((resolve, reject) => {
+  //     writer.on('finish', resolve);
+  //     writer.on('error', reject);
+  //   });
+
+  //   const solvedCaptcha = await captchaSolving(captchaImagePath);
+  //   console.log('Solved captcha:', solvedCaptcha);
+
+  //   await page.waitForSelector('#security_code');
+  //   await page.type('#security_code', solvedCaptcha, { delay: 250 });
+
+  //   await delay(100); 
+  // } else {
+  //   console.error('Captcha image not found.');
+  // }
+
+  await page.click('.uk-button.uk-button-primary.uk-button-large.uk-width-1-1.cn-login-btn.cn-submit1');
+
+  await page.waitForNavigation({ waitUntil: 'networkidle2' });
+  
+  const errorMessage = await page.evaluate(() => {
+    const errorElement = document.querySelector('.error-message');
+    return errorElement ? errorElement.innerText : null;
   });
 
-  if (imageSrc) {
-    console.log('Captcha image found:', imageSrc);
-
-    const captchaImagePath = path.join(__dirname, 'captcha.png');
-    const writer = fs.createWriteStream(captchaImagePath);
-    const response = await axios({
-      url: imageSrc,
-      method: 'GET',
-      responseType: 'stream'
-    });
-
-    response.data.pipe(writer);
-
-    await new Promise((resolve, reject) => {
-      writer.on('finish', resolve);
-      writer.on('error', reject);
-    });
-
-    const solvedCaptcha = await captchaSolving(captchaImagePath);
-    console.log('Solved captcha:', solvedCaptcha);
-
-    await page.waitForSelector('#security_code');
-    await page.type('#security_code', solvedCaptcha, { delay: 250 });
-
-    await delay(100); 
-
-    await page.click('.uk-button.uk-button-primary.uk-button-large.uk-width-1-1.cn-login-btn.cn-submit1');
-
-    await page.waitForNavigation({ waitUntil: 'networkidle2' });
-    
-    const errorMessage = await page.evaluate(() => {
-      const errorElement = document.querySelector('.error-message');
-      return errorElement ? errorElement.innerText : null;
-    });
-
-    if (errorMessage) {
-      console.error('Login failed with message:', errorMessage);
-    } else {
-      console.log('Login successful!');
-    }
+  if (errorMessage) {
+    console.error('Login failed with message:', errorMessage);
   } else {
-    console.error('Captcha image not found.');
+    console.log('Login successful!');
   }
 
   await delay(600000);
